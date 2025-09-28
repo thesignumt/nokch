2d9f32b05b92e368bc9dcf2810dc30c0244a2a35
thesignumt <tcrrxzz@gmail.com>
2025-09-28 11:31:29 +0100

    feat(tokens): position tracking

--- nokch/tokens.py
+++ nokch/tokens.py
@@ -71,14 +71,16 @@ class T(Enum):
 
 
 class Token:
-    def __init__(self, type_: T, val: Any = None) -> None:
+    def __init__(self, type_: T, val: Any = None, *, pos: tuple[int, int]) -> None:
         self.type = type_
         self.val = val
+        self.pos = pos  # (line, col) tuple
 
     def __repr__(self) -> str:
+        line, col = self.pos
         if self.val is not None:
-            return f"Token({self.type}, {self.val})"
-        return f"Token({self.type})"
+            return f"Token({self.type}, {self.val}, pos=({line},{col}))"
+        return f"Token({self.type}, pos=({line},{col}))"
 
 
 class E(str, Enum):
